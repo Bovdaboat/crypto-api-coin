555ab4443c345ebddde0799f10d6284afd2f4da3
bovdaboat <ignatgabov@gmail.com>
2023-07-09 10:33:29 -0700

    first init

---
+++ src/app/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <div className="navbar bg-blue-500">
+            <a className="btn btn-base normal-case text-xl">CoinHive</a>
+        </div>
+    )
+}
+
+export default Navbar

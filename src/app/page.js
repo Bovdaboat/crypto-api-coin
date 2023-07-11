a7bbb968357294473c8e69e5aea9a7172edf0c97
bovdaboat <ignatgabov@gmail.com>
2023-07-10 17:44:06 -0700

    added a footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from "@/app/component/Navbar";
 import Hero from "@/app/component/Hero";
+import Footer from "@/app/component/Footer";
 
 export default function Home() {
   return (
       <>
       <Navbar />
           <Hero />
+          <Footer />
        </>
           )
 
